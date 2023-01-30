74c13557416ea55d02904b22bf91b0471916c813
mirian.khajishvili@gmail.com <mirian.khajishvili@gmail.com>
2023-01-30 22:49:45 +0400

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainLayoutModule } from './core/features/main-layout/main-layout.module';
-import {HomeModule} from "./pages/home/home.module";
+import { HomeModule} from "./pages/home/home.module";
 
 
 
