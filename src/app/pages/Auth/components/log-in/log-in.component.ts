b947ef7f80e192268668d6a4f79677420c466037
mirian.khajishvili@gmail.com <mirian.khajishvili@gmail.com>
2023-02-02 23:43:15 +0400

    add log-in validation

--- src/app/pages/Auth/components/log-in/log-in.component.ts
+++ src/app/pages/Auth/components/log-in/log-in.component.ts
@@ -17,7 +17,7 @@ export class LogInComponent implements OnInit {
 
   form: FormGroup = new FormGroup({
 
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
   })
 
