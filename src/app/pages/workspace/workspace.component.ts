d0c11e8062d25dc1663d5f4443e8fcbfb12c24eb
SalyOne <salome.batkhadze@gmail.com>
2023-02-06 17:06:32 +0400

    add workspace-create component

--- src/app/pages/workspace/workspace.component.ts
+++ src/app/pages/workspace/workspace.component.ts
@@ -10,31 +10,11 @@ import {ThemePalette} from "@angular/material/core";
   styleUrls: ['./workspace.component.scss']
 })
 export class WorkspaceComponent implements OnDestroy{
-  form: FormGroup = new FormGroup({
-    name : new FormControl('', [Validators.required]),
-    abbreviation : new FormControl('', [Validators.required]),
-    description : new FormControl('',[Validators.required]),
-    color : new FormControl('',[Validators.required]),
-    }
-  );
+
  sub$ = new Subject();
 
 
-  constructor(
-    private workspaceService:WorkspaceService
-  ) {
-  }
-  submit(){
-    this.form.markAllAsTouched();
-    if(this.form.invalid) return;
-    console.log(this.form.value)
-    this.workspaceService.addWorkspace(this.form.value)
-      .pipe(takeUntil(this.sub$))
-      .subscribe(res =>{
-        console.log("ress:" +
-          " " , res)
-      })
-  }
+  constructor() { }
 
   ngOnDestroy(): void {
     this.sub$.next(null);
