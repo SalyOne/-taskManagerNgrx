a27eb6612a7b37c79768d6a4bbcb7ffd1e018bd3
SalyOne <salome.batkhadze@gmail.com>
2023-02-09 20:32:02 +0400

    move features folder from core to app folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainLayoutModule } from './core/features/main-layout/main-layout.module';
+import { MainLayoutModule } from './features/main-layout/main-layout.module';
 import { HomeModule} from "./pages/home/home.module";
 import {WorkspaceModule} from "./pages/workspace/workspace.module";
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -31,7 +31,7 @@ import { AuthInterceptor } from './core/interceptors';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true 
+      multi: true
     }
 
   ],
