037978fed2379255221c39206c7cc5f2370918d6
davit <davitijmukhadze@gmail.com>
2023-02-13 23:54:07 +0400

    add:merge dev.

--- src/app/features/main-layout/main-layout.module.ts
+++ src/app/features/main-layout/main-layout.module.ts
@@ -6,6 +6,10 @@ import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatIconModule} from '@angular/material/icon';
 import { MatMenuModule} from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import {MatListModule} from "@angular/material/list";
 
 
 
@@ -14,14 +18,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     MainLayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
     MatSidenavModule,
     MatIconModule,
     MatMenuModule,
-    RouterModule
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule
 
   ],
   exports: [
