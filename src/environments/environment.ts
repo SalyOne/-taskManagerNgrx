5bd1d6cdd4e785a959d62ca64f0404229d04fa2a
SalyOne <salome.batkhadze@gmail.com>
2023-03-23 19:27:37 +0400

    edit environment link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://task-manager-production-40ea.up.railway.app/api/',
+  apiUrl: 'https://task-manager-production-40ea.up.railway.app/api/',
   // apiUrl: 'http://localhost:3000/api/'
   // apiUrl: 'https://seal-app-l3d7u.ondigitalocean.app/'
 };
